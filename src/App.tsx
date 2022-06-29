756814c1dd6b63bfaa4c0637d461b41e682e1f7e
Remi <97021497+Remi-1201@users.noreply.github.com>
2022-06-29 12:13:53 +0900

    解釈を追加

--- src/App.tsx
+++ src/App.tsx
@@ -5,30 +5,39 @@ const labels = ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"];
 
 const skipped = (ctx, value) =>
   ctx.p0.skip || ctx.p1.skip ? value : undefined;
+
 const down = (ctx, value) =>
   ctx.p0.parsed.y > ctx.p1.parsed.y ? value : undefined;
 
 export const options = {
   responsive: true,
+  // 凡例の設定
   plugins: {
     legend: {
+      // rtl = 凡例ボックスと項目名の書き順を逆にする。デフォルトは false (trueで項目名が先に出てくるようにした)
       rtl: true,
+      //凡例を右側に
       position: "right" as const,
+      // 凡例の位置をデータに近づける
       align: "end",
       labels: {
+        // 項目名を表示したいのでtrueに
         display: true,
-        usePointStyle: true,
+        // データと項目名の色を揃える
         color: "rgb(75, 192, 192)",
-        // to hide the label box
+        // 凡例ボックスの幅、ボックスは不要なので【0】で非表示にする
         boxWidth: 0,
+        // 項目名の書式設定
         font: {
           weight: "bold"
         },
         textAlign: "right",
+        // x軸から少し離れるようにpaddingを設定
         padding: 35
       }
     },
     title: {
+      // trueでタイトルを表示に設定
       display: true,
       text: "Line Segment Styling",
       font: {
